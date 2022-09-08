8a23f13603ba8c0c22e3183a03b81910c7fb59df
genccbilal <bilalgnc34@gmail.com>
2022-09-08 21:00:43 +0300

    Admin Modülü
    Kullanıcı giriş konturolü yapıldı

---
+++ Ticari_Otamasyon/Ticari_Otamasyon/frmAdmin.Designer.cs
@@ -0,0 +1,144 @@
+﻿
+namespace Ticari_Otamasyon
+{
+    partial class frmAdmin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmAdmin));
+            this.TxtSifre = new DevExpress.XtraEditors.TextEdit();
+            this.label2 = new System.Windows.Forms.Label();
+            this.TxtKullaniciAd = new DevExpress.XtraEditors.TextEdit();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnGiriş = new DevExpress.XtraEditors.SimpleButton();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.TxtSifre.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.TxtKullaniciAd.Properties)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // TxtSifre
+            // 
+            this.TxtSifre.Location = new System.Drawing.Point(146, 206);
+            this.TxtSifre.Margin = new System.Windows.Forms.Padding(4);
+            this.TxtSifre.Name = "TxtSifre";
+            this.TxtSifre.Properties.Appearance.Font = new System.Drawing.Font("Monotype Corsiva", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TxtSifre.Properties.Appearance.Options.UseFont = true;
+            this.TxtSifre.Properties.UseSystemPasswordChar = true;
+            this.TxtSifre.Size = new System.Drawing.Size(143, 30);
+            this.TxtSifre.TabIndex = 8;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Monotype Corsiva", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(86, 209);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 24);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Şifre:";
+            // 
+            // TxtKullaniciAd
+            // 
+            this.TxtKullaniciAd.Location = new System.Drawing.Point(146, 169);
+            this.TxtKullaniciAd.Margin = new System.Windows.Forms.Padding(4);
+            this.TxtKullaniciAd.Name = "TxtKullaniciAd";
+            this.TxtKullaniciAd.Properties.Appearance.Font = new System.Drawing.Font("Monotype Corsiva", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TxtKullaniciAd.Properties.Appearance.Options.UseFont = true;
+            this.TxtKullaniciAd.Size = new System.Drawing.Size(143, 30);
+            this.TxtKullaniciAd.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Monotype Corsiva", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(17, 172);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(111, 24);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Kullanıcı Adı:";
+            // 
+            // btnGiriş
+            // 
+            this.btnGiriş.Appearance.Font = new System.Drawing.Font("Monotype Corsiva", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnGiriş.Appearance.Options.UseFont = true;
+            this.btnGiriş.Location = new System.Drawing.Point(158, 243);
+            this.btnGiriş.Name = "btnGiriş";
+            this.btnGiriş.Size = new System.Drawing.Size(121, 44);
+            this.btnGiriş.TabIndex = 9;
+            this.btnGiriş.Text = "Giriş Yap";
+            this.btnGiriş.Click += new System.EventHandler(this.btnGiriş_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Monotype Corsiva", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(174, 398);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(233, 24);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "TİCARİ OTOMASYON - 2018";
+            // 
+            // frmAdmin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(691, 431);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnGiriş);
+            this.Controls.Add(this.TxtSifre);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.TxtKullaniciAd);
+            this.Controls.Add(this.label1);
+            this.DoubleBuffered = true;
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "frmAdmin";
+            this.Text = "frmAdmin";
+            ((System.ComponentModel.ISupportInitialize)(this.TxtSifre.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.TxtKullaniciAd.Properties)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private DevExpress.XtraEditors.TextEdit TxtSifre;
+        private System.Windows.Forms.Label label2;
+        private DevExpress.XtraEditors.TextEdit TxtKullaniciAd;
+        private System.Windows.Forms.Label label1;
+        private DevExpress.XtraEditors.SimpleButton btnGiriş;
+        private System.Windows.Forms.Label label3;
+    }
+}
---
+++ Ticari_Otamasyon/Ticari_Otamasyon/frmAdmin.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Ticari_Otamasyon
+{
+    public partial class frmAdmin : Form
+    {
+        public frmAdmin()
+        {
+            InitializeComponent();
+        }
+
+        sqlBaglanti bgl = new sqlBaglanti();
+
+        private void btnGiriş_Click(object sender, EventArgs e)
+        {
+            SqlCommand komut = new SqlCommand("SELECT *FROM TBL_ADMIN WHERE KULLANICIAD=@k1 AND SIFRE=@k2", bgl.baglanti());
+            komut.Parameters.AddWithValue("@k1", TxtKullaniciAd.Text);
+            komut.Parameters.AddWithValue("@k2", TxtSifre.Text);
+            SqlDataReader da = komut.ExecuteReader();
+            if (da.Read())
+            {
+                Form1 frm = new Form1();
+                frm.Show();
+            }
+
+        }
+    }
+}
